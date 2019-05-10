a13d2b24f2bbf3f3f7d8a011a95b9a3eaed54583
Sylwek <Sylwek>
2019-05-10 12:11:35 +0100

    [ADD] - Add model classes

---
+++ app/src/main/java/com/example/albumretrofit/model/Album.kt
@@ -0,0 +1,8 @@
+package com.example.albumretrofit.model
+
+/**
+ * Created by Sylwek on 10/05/2019.
+ * AlbumRetrofit
+ */
+data class Album (val userId: Int, val id: Int, val title: String){
+}
---
+++ app/src/main/java/com/example/albumretrofit/model/Photo.kt
@@ -0,0 +1,8 @@
+package com.example.albumretrofit.model
+
+/**
+ * Created by Sylwek on 10/05/2019.
+ * AlbumRetrofit
+ */
+data class Photo (val albumId: Int, val id: Int, val title: String,val url: String,val thumbnailUrl: String){
+}
